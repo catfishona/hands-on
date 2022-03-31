b85d64676eb4dd8cd6c1768441ef6425f8bd54f5
catfishona <1972034@maranatha.ac.id>
2022-03-31 13:47:24 +0800

    change to hooks

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,74 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { ImageComponent } from "components/Gif";
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchQuery: "",
-      searchResults: [],
-      theresData: false,
-    };
-  }
+function Search() {
+  const [gifs, setGifs] = useState([]);
+  const [query, setQuery] = useState("");
+  const [found, isFound] = useState(false);
 
-  getGifs = (query) => {
-    axios
+  const getGifs = async (query) => {
+    const datas = await axios
       .get(
-        `https://api.giphy.com/v1/gifs/search?api_key=Dug5OETNnJbpXFty4H3sStaKFoYlN5HM&q=${query}&limit=12`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
+        `https://api.giphy.com/v1/gifs/search?api_key=Dug5OETNnJbpXFty4H3sStaKFoYlN5HM&q=${query}&limit=12`
       )
       .then((response) => {
-        this.setState({ searchResults: response.data.data });
-        const gifs = this.state.searchResults;
-        console.log(gifs);
-        gifs ? this.setState({ theresData: true }) : console.log("no");
-        // console.log(this.state.theresData);
+        return response.data.data;
       })
       .catch((e) => console.log(e));
+
+    console.log(datas);
+
+    // check if data is found
+    datas.length > 0 ? isFound(true) : isFound(false);
+    //TODO: found still returns false
+    // found ? console.log("data found !") : console.log("no data found");
+
+    setGifs(datas);
   };
 
-  getInput = (e) => {
-    this.setState({ searchQuery: e.target.value });
+  const getInput = (e) => {
+    setQuery(e.target.value);
   };
 
-  searchInput = (e) => {
-    console.log(`Query: ${this.state.searchQuery}`);
-    this.getGifs(this.state.searchQuery);
+  const searchInput = (e) => {
     e.preventDefault();
+    console.log(`query: ${query}`);
+    getGifs(query);
   };
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.searchInput}>
-          <input
-            className="input-area"
-            type="text"
-            placeholder="Search for a gif"
-            value={this.state.searchQuery}
-            onChange={this.getInput}
-          ></input>
-          <button type="submit">Submit</button>
-          {this.state.theresData ? (
-            this.state.searchResults.map((gif, id) => (
-              <ImageComponent
-                key={id}
-                url={gif.images.fixed_height.url}
-                title={gif.title}
-              />
-            ))
-          ) : (
-            <p>No data</p>
-          )}
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <form onSubmit={searchInput}>
+        <input
+          className="input-area"
+          type="text"
+          placeholder="Search for a gif"
+          value={query}
+          onChange={getInput}
+        ></input>
+        <button type="submit">Search</button>
+
+        {found ? (
+          gifs.map((gif, id) => (
+            <ImageComponent
+              key={id}
+              url={gif.images.fixed_height.url}
+              title={gif.title}
+            />
+          ))
+        ) : (
+          <p>No data to show</p>
+        )}
+      </form>
+
+      {gifs.map((gif, id) => (
+        <ImageComponent
+          key={id}
+          url={gif.images.fixed_height.url}
+          title={gif.title}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Search;
