f5584983c60ee49138e21c2f29508498df888f58
catfishona <1972034@maranatha.ac.id>
2022-04-05 20:50:29 +0800

    pass provider component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "App.css";
 // import Home from "pages/home";
 import Search from "pages/search";
+import { Provider } from "react-redux";
+import store from "redux/store";
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
+      <Provider store={store}>
       <Search />
+
+      </Provider>
     </div>
   );
 }
